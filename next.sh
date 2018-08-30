8fda24a0c1da14c703ac14549c644c502744a672
david <david@davidberry.io>
2018-08-30 09:21:15 -0700

    looping shell script upgrade using ssh connection

--- next.sh
+++ next.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-echo "Updating code for user $1"
-git remote add $1 https://github.com/$1/hrext-twittler
-echo "Git remote added"
-git fetch $1 master
-echo "Master fetched"
-git reset --hard $1/master
+Students=( 'leemichelle' 'Mikaylahasal' 'MikeTemme' 'minak66' 'pat-doyle' 'rockyd8' 'sean-bellinger' 'Stephaniee2' 'stevenchung213' 'sveta0112' 'taylorhasal' 'datusername1' 'vyoung415' 'vintg' 'vvek87' )
+for i in "${Students[@]}"; do
+    mkdir $i
+    cd $i
+    git init
+    echo "Updating code for user $i"
+    git remote add $i git@github.com:$i/hrext-twittler
+    echo "Git remote added"
+    git fetch $i master
+    echo "Master fetched"
+    git reset --hard $i/master
+    cd ..
+done
